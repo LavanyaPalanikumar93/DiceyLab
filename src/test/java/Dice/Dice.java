b06bd04f1871798b49539be21f2cde83e369d06a
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-04-04 21:16:41 -0400

    Done

--- src/test/java/Dice/Dice.java
+++ src/test/java/Dice/Dice.java
@@ -1,12 +1,13 @@
 package Dice;
+import org.junit.Assert;
 import org.junit.Test;
 public class Dice {
     @Test
-    public int testRoll(){
+    public void testRoll(){
         int numberOfDice=2;
-        Dice dice=new Dice(numberOfDice);
-        int max=numberOfDice*6;
-       Integer actual=dice.roll();
+        Dice dice=new Dice();
+        int total=dice.roll
+        Assert.assertTrue(to);
 
     }
 
